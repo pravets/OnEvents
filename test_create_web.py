ab9bfabfad0ae8d4a39945255945434e9320d47a
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2025-10-01 21:54:57 +0300

    Покрытие тестами

--- test_create_web.py
+++ test_create_web.py
@@ -1,5 +1,8 @@
-from create_web import clean_text
+from create_web import clean_text, to_hhmmss
 
 def test_clean_text():
     assert clean_text("<br>") == ""
     assert clean_text("<a>hello</a>") == "hello"
+
+def test_to_hhmmss():
+    assert to_hhmmss("22:00") == "220000"
